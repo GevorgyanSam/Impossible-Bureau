f993151ed8f14dc5fc7e94589532d658c36a15fc
GevorgyanSam <mylaptopacer.01182005@gmail.com>
2022-08-09 21:36:37 +0300

    Finished One of The News Pages

---
+++ src/js/mission.js
@@ -0,0 +1,41 @@
+"use strict";
+
+$(function () {
+
+
+
+
+                        // NAVIGATION
+
+
+
+                        
+    $(".navIcon2").click(() => {
+
+        $("nav").css({
+            display: "block"
+        })
+
+        $(".navRow").css({
+            display: "none"
+        })
+
+    })
+
+    $(".navIcon").click(() => {
+
+        $("nav").css({
+            display: "none",
+        })
+
+        $(".navRow").css({
+            display: "flex"
+        })
+
+    })
+
+    $("#submit").click((e) => {
+        e.preventDefault()
+    })
+
+})
