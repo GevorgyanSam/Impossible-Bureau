195773dcb15dd709e16db789198d0a804e05289c
GevorgyanSam <mylaptopacer.01182005@gmail.com>
2022-08-13 16:16:20 +0300

    Finished The 'Project' Page in Primary Work page

--- src/js/project.js
+++ src/js/project.js
@@ -38,16 +38,17 @@ $(function () {
         e.preventDefault()
     })
 
-    if ($(window).width() < 900) {
-        $(".bg-paralax2").removeClass("scroll-bg2")
+    if ($(window).width() < 940) {
+        $(".bg-paralax2").removeClass("scroll-bg2");
+        $(".bg-paralax").removeClass("scroll-bg1")
     }
 
     $(window).scroll(function() {
         
         let wScroll = $(window).scrollTop();
 
-        $(".bg-paralax").css({
-            backgroundPosition: "center " + (wScroll * 0.25) + 'px',
+        $(".scroll-bg1").css({
+            backgroundPosition: "center " + (wScroll * 0.25 - 100) + 'px',
         })
 
         $(".scroll-bg2").css({
