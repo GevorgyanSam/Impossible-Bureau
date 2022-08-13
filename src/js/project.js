3b25a879fb973420d1fbe57681ffe761bff28465
GevorgyanSam <mylaptopacer.01182005@gmail.com>
2022-08-13 12:03:58 +0300

    Added two sections with animations & JQuery Parallax effect

--- src/js/project.js
+++ src/js/project.js
@@ -38,12 +38,20 @@ $(function () {
         e.preventDefault()
     })
 
+    if ($(window).width() < 900) {
+        $(".bg-paralax2").removeClass("scroll-bg2")
+    }
+
     $(window).scroll(function() {
         
         let wScroll = $(window).scrollTop();
 
         $(".bg-paralax").css({
             backgroundPosition: "center " + (wScroll * 0.25) + 'px',
+        })
+
+        $(".scroll-bg2").css({
+            backgroundPosition: "center " + (wScroll * 0.25 - $(".bg-paralax2").height() / 2) + 'px',
         })
 
     })
